490de1e3e6a6a327c8f3bc56b115b95c35a9d77d
Yeasin-Hossen-Arman <yeasinhossenarman@gmail.com>
2024-04-03 20:37:46 +0600

    file add

---
+++ edgeDEctection.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import numpy as np
+
+def Edge_detection(frame):
+    #image read and show
+    frame = cv.imread('house.jpg')
+    cv.imshow('your given img',frame)
+
+    #concertion rgb image to gray scale image
+    gray_img = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    cv.imshow('gray img', gray_img)
+
+    #blur images using gaussian kernal 
+    blur_img = cv.GaussianBlur(gray_img,(3,3),cv.BORDER_DEFAULT)
+    cv.imshow('blur img', blur_img)
+
+#blur images using gaussian kernal (3,3)
+# blur_img3 = cv.GaussianBlur(gray_img,(3,3),cv.BORDER_DEFAULT)
+# cv.imshow('blur img 3 3', blur_img3)
+
+    #Edge cascade
+    canny_img = cv.Canny(blur_img,125,275)
+    cv.imshow('canny img, final img', canny_img)
+
+    #Dilating the image
+    dilate_img = cv.dilate(canny_img, (5,5), iterations=1)
+    cv.imshow('Dilate img', dilate_img)
+
+    #eroding image
+    erode_img = cv.erode(dilate_img, (3,3), iterations=1)
+    cv.imshow('Erode img',erode_img)
+
+#Edge cascade from 3,3
+# canny_img3 = cv.Canny(blur_img3,125,175)
+# cv.imshow('canny img3', canny_img3)
+
+
+
+Edge_detection('house.jpg')
+
+cv.waitKey(0)
+cv.destroyAllWindows()
